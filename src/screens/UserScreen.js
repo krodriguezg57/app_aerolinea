2e9ca949d75349459cae9b9e9157c15cf2f5b1a5
krodriguezg57 <krodriguez57@gmail.com>
2025-04-04 22:02:12 -0500

    Closes #2: implementado login, registro y conexión con Firebase

--- src/screens/UserScreen.js
+++ src/screens/UserScreen.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { View, Text, StyleSheet} from 'react-native'
 import { FooterComponent } from './shared/FooterComponent'; 
+import { useAuth } from '../context/AuthContext';
 
-export const UserScreen = () => {
+export const UserScreen = ({navigation}) => {
+  const {user}=useAuth()
   return (
     <View style ={styles.container}>
+        <Text>{user?.displayName|| "Usuario"}</Text>
         <Text>Hola UserScreen</Text>
         <FooterComponent />
     </View>
